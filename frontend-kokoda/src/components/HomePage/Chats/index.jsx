9424991584e595f57cf3b90915e7b7ec26ecc457
boskoa <bosko.ap@gmail.com>
2024-12-14 12:11:26 +0100

    Reorganizing files structure

--- frontend-kokoda/src/components/HomePage/Chats/index.jsx
+++ frontend-kokoda/src/components/HomePage/Chats/index.jsx
@@ -1,7 +1,59 @@
-import AllChats from "../../../features/chats/AllChats";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getAllChats,
+  selectAllChats,
+  selectChatsLoading,
+} from "../../../features/chats/chatsSlice";
+import styled from "styled-components";
+import SingleChat from "./SingleChat";
+import { useEffect, useRef, useState } from "react";
+import Spinner from "../../Spinner";
+import useIntersectionObserver from "../../../customHooks/useIntersectionObserver";
 
-function Chats() {
-  return <AllChats />;
+const AllChatsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 5px;
+  width: 100%;
+  justify-content: start;
+`;
+
+function AllChats() {
+  const chats = useSelector(selectAllChats);
+  const endRef = useRef(null);
+  const intersecting = useIntersectionObserver(endRef);
+  const dispatch = useDispatch();
+  const [offset, setOffset] = useState(chats.length);
+  const loading = useSelector(selectChatsLoading);
+  const limit = 20;
+
+  useEffect(() => {
+    document.getElementById("vp").scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
+  useEffect(() => {
+    if (intersecting && chats.length % limit === 0 && chats.length >= offset) {
+      dispatch(
+        getAllChats({
+          token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJ0ZXN0IiwiaWF0IjoxNzMzMDA3NTc2fQ.y8vfgSOqsPf1TxBn1SxjFkjKMSW3BsNOJdXdZDd28l0",
+          offset,
+          limit,
+        }),
+      );
+      setOffset((p) => p + limit);
+    }
+  }, [intersecting]);
+
+  return (
+    <AllChatsContainer>
+      {chats.map((c) => (
+        <SingleChat key={c.id} chat={c} />
+      ))}
+      <Spinner endRef={endRef} loading={loading} />
+    </AllChatsContainer>
+  );
 }
 
-export default Chats;
+export default AllChats;
--- frontend-kokoda/src/components/HomePage/Contacts/index.jsx
+++ frontend-kokoda/src/components/HomePage/Contacts/index.jsx
@@ -1,7 +1,65 @@
-import AllContacts from "../../../features/contacts/AllContacts";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import {
+  getAllContacts,
+  selectAllContacts,
+  selectContactsLoading,
+} from "../../../features/contacts/contactsSlice";
+import SingleContact from "./SingleContact";
+import { useEffect, useRef, useState } from "react";
+import Spinner from "../../Spinner";
+import useIntersectionObserver from "../../../customHooks/useIntersectionObserver";
 
-function Contacts() {
-  return <AllContacts />;
+const AllContactsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 5px;
+  width: 100%;
+  justify-content: start;
+`;
+
+function AllContacts() {
+  const contacts = useSelector(selectAllContacts);
+  const endRef = useRef(null);
+  const intersecting = useIntersectionObserver(endRef);
+  const dispatch = useDispatch();
+  const [offset, setOffset] = useState(contacts.length);
+  const loading = useSelector(selectContactsLoading);
+  const limit = 20;
+
+  useEffect(() => {
+    document.getElementById("vp").scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
+  useEffect(() => {
+    if (
+      intersecting &&
+      contacts.length % limit === 0 &&
+      contacts.length >= offset
+    ) {
+      dispatch(getAllContacts({ offset, limit }));
+      setOffset((p) => p + limit);
+    }
+  }, [intersecting]);
+
+  return (
+    <AllContactsContainer>
+      {contacts
+        .concat({
+          name: "dsfdsf sdfdsf ghhrth sdsadasd frewtret sdfdsfgr ezhrthzzujuj",
+          id: 234,
+          company: {
+            catchPhrase:
+              "YABADABADU YABADABADU YABADABADU YABADABADU YABADABADU YABADABADUYABADABADUYABADABADU YABADABADU YABADABADU YABADABADU",
+          },
+        })
+        .map((c) => (
+          <SingleContact key={c.id} contact={c} />
+        ))}
+      <Spinner endRef={endRef} loading={loading} />
+    </AllContactsContainer>
+  );
 }
 
-export default Contacts;
+export default AllContacts;
